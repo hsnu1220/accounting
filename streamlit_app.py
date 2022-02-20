44c03cc474ef2bd31b559eeb088c5f0d1e0b519e
hsnu1220 <hsnu1220@gmail.com>
2022-02-20 16:57:41 +0800

    add table of contents in sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,9 @@ from plotly import express as px
 
 
 ### ===== global variables ===== ###
+hdr_out = '支出'
+hdr_total = '滿月記帳表'
+
 ym_format = '%Y/%m'
 date_format = f'{ym_format}/%d'
 col_ym = '年/月'
@@ -22,17 +25,7 @@ cols_sheet = [col_date, col_store, col_item, col_amount, col_class]
 hues = px.colors.qualitative.Safe
 
 
-### ===== page config ===== ###
-st.set_page_config(
-    page_title='滿月記帳表',
-    page_icon=':whale:',
-    layout='centered',
-    initial_sidebar_state='collapsed'
-)
-st.title('布基帳')
-
-
-### ===== get google sheet ===== ###
+### ===== helper functions ===== ###
 # @st.cache(suppress_st_warning=True)
 def get_google_sheet(serial_num, name):
    end_point = 'https://docs.google.com/spreadsheets/d'
@@ -51,21 +44,40 @@ def get_google_sheet(serial_num, name):
    return df
 
 
+### ===== page config ===== ###
+st.set_page_config(
+    page_title='布基帳',
+    page_icon=':whale:',
+    layout='centered',
+    initial_sidebar_state='collapsed'
+)
+
+
+### ===== sidebar ===== ###
+st.sidebar.header('比較範圍')
+num_months = int(st.sidebar.text_input(label='月', value='3'))
+
+st.sidebar.markdown(
+   f'''
+   ## 目錄
+   - [{hdr_out}](#{hdr_out})
+   - [{hdr_total}](#{hdr_total})
+   ''',
+   unsafe_allow_html=True
+)
+
+
+### ===== spending ===== ###
+st.title(hdr_out)
 df_raw = get_google_sheet(
    serial_num='1WgZrSv9pNPyAd7IutFJ43aB5UX8KiWGGSEfHK76xoS4',
    name='總表'
 )
 
-
-### ===== summarize by month ===== ###
-st.sidebar.header('比較範圍')
-num_months = int(st.sidebar.text_input(label='月', value='3'))
-
-
-### ===== monthly total ===== ###
+# monthly total
 st.header('逐月總支出')
 df_month = df_raw.groupby(by=col_ym, as_index=False)[col_amount].agg('sum')
-fig_line = go.Figure(go.Scatter(
+fig_month_total = go.Figure(go.Scatter(
    name=col_amount,
    mode='markers',
    x=df_month[col_ym],
@@ -74,7 +86,7 @@ fig_line = go.Figure(go.Scatter(
       size=16
    )
 ))
-fig_line.add_trace(go.Scatter(
+fig_month_total.add_trace(go.Scatter(
    name=f'過去{num_months}個月移動平均',
    mode='lines',
    x=df_month[col_ym],
@@ -83,13 +95,12 @@ fig_line.add_trace(go.Scatter(
       width=8
    )
 ))
-st.plotly_chart(fig_line)
+st.plotly_chart(fig_month_total)
 
-
-### ===== pie chart by class ===== ###
+# recent few months by class
 st.header(f'過去{num_months}個月')
 ym_list = df_raw[col_ym].unique().tolist()
-fig_pie = make_subplots(
+fig_recent_class = make_subplots(
    rows=1, cols=num_months,
    specs=[[{'type': 'pie'} for _ in range(num_months)]],
    subplot_titles=ym_list[-num_months:]
@@ -98,7 +109,7 @@ for idx, ym in enumerate(ym_list[-num_months:]):
    df_month = df_raw[df_raw[col_ym] == ym]
    df_class = df_month.groupby(by=col_class, as_index=False)[col_amount].agg('sum')
 
-   fig_pie.add_trace(
+   fig_recent_class.add_trace(
       go.Pie(
          labels=df_class[col_class],
          values=df_class[col_amount],
@@ -111,10 +122,9 @@ for idx, ym in enumerate(ym_list[-num_months:]):
       ),
       row=1, col=(idx + 1)
    )
-st.plotly_chart(fig_pie)
+st.plotly_chart(fig_recent_class)
 
-
-### ===== single-month detail ===== ###
+# monthly detail
 st.header('單月類別')
 ym = st.selectbox(label='年/月', options=ym_list, index=len(ym_list)-1)
 df_month = df_raw[df_raw[col_ym] == ym]
@@ -124,14 +134,18 @@ with st.expander('明細'):
    df_month.index = df_month.index.strftime(date_format)
    st.table(df_month.drop(columns=[col_ym]))
 
-
-### ===== monthly bar chart ===== ###
+# every month by class
 st.header('逐月類別')
-fig_bar = px.bar(
+fig_month_class = px.bar(
    data_frame=df_raw,
    x=col_ym,
    y=col_amount,
    color=col_class,
    color_discrete_sequence=hues
 )
-st.plotly_chart(fig_bar)
+st.plotly_chart(fig_month_class)
+
+
+### ===== monthly accounting ===== ###
+st.header(hdr_total)
+st.write('WIP')
